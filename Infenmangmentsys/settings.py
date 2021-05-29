0681ea386a0bf4283971affd9dfe1f927c912baa
Mahmoud Arobie <41952155+M98abas@users.noreply.github.com>
2021-05-29 11:06:23 +0300

    change-settings

--- Infenmangmentsys/settings.py
+++ Infenmangmentsys/settings.py
@@ -76,35 +76,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Infenmangmentsys.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-    
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-
-        'NAME': 'invmansys',
-
-        'USER': 'mahmoudab',
-
-        'PASSWORD': 'Mahmoud@1998',
-
-        'HOST': 'localhost',
-
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
